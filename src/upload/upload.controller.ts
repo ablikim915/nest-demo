4f15b23c9a0264cd850fa3e87e9589522a06ae06
ablikim <rexiti@souche.com>
2023-03-06 17:05:05 +0800

    feat: 下载文件和文件流

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Post, Get, UseInterceptors, UploadedFile, Res } from '@nestjs/common';
 import { UploadService } from './upload.service';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from "express";
+import { join } from 'path';
+import { zip } from 'compressing';
 
 @Controller('upload')
 export class UploadController {
@@ -8,10 +11,29 @@ export class UploadController {
 
   @Post('album')
   @UseInterceptors( // 处理上传文件
-    FileInterceptor('file') // 定义接口字段名称
+    FileInterceptor('file') // 定义请求参数名称
   )
   upload(@UploadedFile() file) {
     console.log('--file--', file)
     return '上传成功'
+  }
+
+  @Get('download')
+  download(@Res() res:Response) {
+    const url = join(__dirname, '../images/1678088451052.png');
+    res.download(url) 
+  }
+
+  // 文件流的方式下载文件
+  @Get('stream')
+  stream(@Res() res:Response) {
+    const url = join(__dirname, '../images/1678088451052.png');
+    const targetStream = new zip.Stream();
+    targetStream.addEntry(url);
+
+    res.setHeader('Content-Type', 'application/octet-stream')
+    res.setHeader('Content-Dispoition', 'attachment; filename=legolas')
+
+    targetStream.pipe(res)
   }
 }
