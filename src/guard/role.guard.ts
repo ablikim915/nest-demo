7c9027eb6c83e7afa07a00d28fe79003b4cb77d9
ablikim <1057908611@qq.com>
2023-03-14 23:17:46 +0800

    chore: 路由守卫方法中增加注释

--- src/guard/role.guard.ts
+++ src/guard/role.guard.ts
@@ -1,20 +1,30 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
-import type { Request } from 'express'
+import type { Request } from 'express';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(private readonly Reflector: Reflector) {}
+
+  // canActivate() 方法是 `CanActivate` 接口的实现方法，它接收一个 `ExecutionContext` 参数，
+  // 并返回一个布尔值、Promise 或 Observable，用于指示路由是否可以被激活
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const admin = this.Reflector.get<string[]>('role', context.getHandler()) // 获取首位role的值
-    const req = context.switchToHttp().getRequest<Request>() // 获取请求实例
     
+    // 从 Reflector 服务中获取具有 `role` 属性的元数据
+    const admin = this.Reflector.get<string[]>('role', context.getHandler());
+    
+    // 从执行上下文中获取 HTTP 请求对象，并将其转换为 Express 请求对象
+    const req = context.switchToHttp().getRequest<Request>(); 
+    
+    // 检查请求的角色是否与 `admin` 变量中的任何角色匹配
     if (admin && admin.includes(req.query.role as string)) {
-      return true
-    } 
-    return false
+      return true;
+    }
+    
+    // 如果请求的角色与 `admin` 变量中的任何角色都不匹配，则返回 false
+    return false;
   }
 }
